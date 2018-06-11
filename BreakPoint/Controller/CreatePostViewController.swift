ab8fc971126c1c3391b8eba98c5f02714d5c78b8
Henry Chukwu <henry.chukwu@andela.com>
2018-06-11 10:23:09 +0100

    Set up authentication and data services

---
+++ BreakPoint/Controller/CreatePostViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CreatePostViewController.swift
+//  BreakPoint
+//
+//  Created by Henry Chukwu on 6/8/18.
+//  Copyright © 2018 Henry Chukwu. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendBtn: UIButton!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+
+    }
+
+    @IBAction func sendBtnWasPressed(_ sender: Any) {
+        if textView.text != nil && textView.text != " Say something here..." {
+            sendBtn.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text!, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil, sendComplete: { (isComplete) in
+                if isComplete {
+                    self.sendBtn.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendBtn.isEnabled = true
+                    print("There was an error")
+                }
+            })
+        }
+    }
+
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
+
+extension CreatePostViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
---
+++ BreakPoint/Controller/MeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MeViewController.swift
+//  BreakPoint
+//
+//  Created by Henry Chukwu on 6/8/18.
+//  Copyright © 2018 Henry Chukwu. All rights reserved.
+//
+
+import UIKit
+
+class MeViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+    }
+}
