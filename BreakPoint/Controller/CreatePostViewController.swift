889c7c80656c8a1d0428ddd93493fb49d342bfa0
Henry Chukwu <henry.chukwu@andela.com>
2018-06-13 16:35:44 +0100

    create groupsVC and userCell

--- BreakPoint/Controller/CreatePostViewController.swift
+++ BreakPoint/Controller/CreatePostViewController.swift
@@ -23,6 +23,11 @@ class CreatePostViewController: UIViewController {
         sendBtn.bindToKeyboard()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.emailLbl.text = Auth.auth().currentUser?.email
+    }
+
     @IBAction func sendBtnWasPressed(_ sender: Any) {
         if textView.text != nil && textView.text != " Say something here..." {
             sendBtn.isEnabled = false
