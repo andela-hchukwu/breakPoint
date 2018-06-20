d1873c9728bb5e7270b00a2b52500a23aec60aaa
Henry Chukwu <henry.chukwu@andela.com>
2018-06-20 18:01:36 +0100

    groupModel and getting all groups from firebase

--- BreakPoint/Controller/GroupsViewController.swift
+++ BreakPoint/Controller/GroupsViewController.swift
@@ -12,10 +12,23 @@ class GroupsViewController: UIViewController {
 
     @IBOutlet weak var groupsTableView: UITableView!
 
+    var groupsArray = [Group]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         groupsTableView.delegate = self
         groupsTableView.dataSource = self
+
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+            DataService.instance.getAllGroups { (returnedGroupsArray) in
+                self.groupsArray = returnedGroupsArray
+                self.groupsTableView.reloadData()
+            }
+        }
     }
 
 
@@ -28,12 +41,13 @@ extension GroupsViewController: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return groupsArray.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "groupCell") as? GroupCell else { return UITableViewCell() }
-        cell.configureCell(title: "Nerd Nerd", description: "The nerdest nerd around", memberCount: 3)
+        let group = groupsArray[indexPath.row]
+        cell.configureCell(title: group.groupTitle, description: group.groupDesc, memberCount: group.memberCount)
         return cell
     }
 }
